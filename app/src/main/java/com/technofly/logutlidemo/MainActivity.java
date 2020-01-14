8ae21a39df2a9841a13bc538313361185ce023dd
takmanthan <takmanthan0@gmail.com>
2020-01-14 17:17:58 +0530

    Create first library for pdf to book

--- app/src/main/java/com/technofly/logutlidemo/MainActivity.java
+++ app/src/main/java/com/technofly/logutlidemo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.technofly.logutlidemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.technofly.technicalbabalogutil.PDFUtils;
+import com.technofly.technicalbabalogutil.PdfUrlToBookPagerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ShowPDFClick(View view) {
+        PDFUtils.downloadDirectory = "My Downloads";
+        String url = "https://www.tutorialspoint.com/cprogramming/cprogramming_tutorial.pdf";
+        startActivity(new Intent(this, PdfUrlToBookPagerActivity.class).putExtra(PDFUtils.PDFUrl,url));
     }
 }
---
+++ technicalbabalogutil/src/main/java/com/technofly/technicalbabalogutil/task/OnDownloadCompleteListner.java
@@ -0,0 +1,8 @@
+package com.technofly.technicalbabalogutil.task;
+
+import java.io.File;
+
+public interface OnDownloadCompleteListner {
+
+    public void onPdfDownload(File file);
+}
